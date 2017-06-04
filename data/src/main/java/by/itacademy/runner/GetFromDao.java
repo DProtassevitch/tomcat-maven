639a29300371d8c16b5b5f9680154392a92bb7e8
DProtassevitch <denis.protassevitch@gmail.com>
2017-06-04 16:12:05 +0300

    Deleted line

---
+++ data/src/main/java/by/itacademy/runner/GetFromDao.java
@@ -0,0 +1,24 @@
+package by.itacademy.runner;
+
+import by.itacademy.dao.UserDao;
+import lombok.Getter;
+
+@Getter
+public class GetFromDao {
+    public String GetDao() {
+        UserDao userDao = new UserDao();
+        String nameDao;
+        String genderDao;
+        String creationTimeDao;
+
+
+        nameDao = userDao.getDefaultUsername().getName();
+        genderDao = userDao.getDefaultUsername().getGender().toString();
+        creationTimeDao = userDao.getDefaultUsername().getCreationDate().toString();
+        System.out.println("Name= " + nameDao + ", Gender: " + genderDao + ", Creation Time: " + creationTimeDao);
+
+        return "Name: " + nameDao + ", Gender: " + genderDao + ", Creation Time: " + creationTimeDao;
+    }
+
+
+}
